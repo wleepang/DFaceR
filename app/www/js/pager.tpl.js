3a9ca4b8f2d27d3aee90ff61279c21ffbd094095
Lee Pang <wleepang@gmail.com>
2015-10-10 11:03:13 -0700

    remove obsolete pager js files

--- app/www/js/pager.tpl.js
+++
@@ -1,6 +0,0 @@
-// delegate a click event handler for pager page-number buttons
-$(document).on("click", "#{{pager_buttons}} button.page-num-button", function() {
-  var page_num = $(this).data("page-num");
-  $("#{{num_current_page}}").val(page_num);
-  $("#{{num_current_page}}").change();
-});
